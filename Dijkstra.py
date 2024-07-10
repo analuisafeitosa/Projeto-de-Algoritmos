10e383c607f28aa87df94e6b679d23db465448dd
analuisafeitosa <analuisafguimaraes@hotmail.com>
2024-07-10 15:12:06 -0300

    Dijkstras linkado ao banco de dados

--- Dijkstra.py
+++ Dijkstra.py
@@ -1,40 +1,52 @@
 import heapq
+from acessoaobanco import acessoaobd
 
 def dijkstra(graph, start, end):
-    queue = [(0, start)]
-    distances = {vertex: float('infinity') for vertex in graph}
+    queue = []
+    heapq.heappush(queue, (0, start))
+    distances = {node: float('infinity') for node in graph}
     distances[start] = 0
-    shortest_path = {vertex: None for vertex in graph}
+    previous_nodes = {node: None for node in graph}
 
     while queue:
-        current_distance, current_vertex = heapq.heappop(queue)
+        current_distance, current_node = heapq.heappop(queue)
 
-        if current_vertex == end:
-            break
-
-        if current_distance > distances[current_vertex]:
+        if current_distance > distances[current_node]:
             continue
 
-        for neighbor, weight in graph[current_vertex].items():
+        for neighbor, weight in graph[current_node].items():
             distance = current_distance + weight
 
             if distance < distances[neighbor]:
                 distances[neighbor] = distance
+                previous_nodes[neighbor] = current_node
                 heapq.heappush(queue, (distance, neighbor))
-                shortest_path[neighbor] = current_vertex
 
-    path = []
-    current_vertex = end
-    while current_vertex is not None:
-        path.append(current_vertex)
-        current_vertex = shortest_path[current_vertex]
-    path = path[::-1]  # Reverso do caminho
+    path, current_node = [], end
+    while previous_nodes[current_node] is not None:
+        path.insert(0, current_node)
+        current_node = previous_nodes[current_node]
+    if path:
+        path.insert(0, current_node)
+    return path, distances[end]
 
-    return distances, path
+def build_graph(data):
+    graph = {}
+    for edge in data:
+        u, v, w = edge
+        if u not in graph:
+            graph[u] = {}
+        if v not in graph:
+            graph[v] = {}
+        graph[u][v] = w
+        graph[v][u] = w  # If the graph is undirected
+    return graph
 
-# Receber os nós de entrada do usuário
-start_vertex = int(input("Digite o nó de origem: "))
-end_vertex = int(input("Digite o nó de destino: "))
-distances, path = dijkstra(graph, start_vertex, end_vertex)
-print(f"Distâncias: {distances}")
-print(f"Caminho mais curto de {start_vertex} para {end_vertex}: {path}")
+data = acessoaobd()
+
+graph = build_graph(data)
+start = int(input("Digite o nó inicial: "))
+end = int(input("Digite o nó final: "))
+
+path, distance = dijkstra(graph, start, end)
+print(f"O menor caminho do nó {start} ao nó {end} é: {path} com distância total de {distance}")
